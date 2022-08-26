02297c795106917c28997d90b18f30bea3a2c7a7
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-08-27 01:12:26 +0530

    Event listener on load

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const img = document.querySelector('#img');
 
 
 button.addEventListener('click', (e) => {
-    e.preventDefault();
-    const str = box.value;
-    img.src = `https://github-readme-stats.vercel.app/api/top-langs?username=${str}&show_icons=true&locale=en`
-    img.hidden = false;
+  e.preventDefault();
+  const str = box.value;
+  img.src = `https://github-readme-stats.vercel.app/api/top-langs?username=${str}&show_icons=true&locale=en`
+  img.hidden = false;
 });
 
-window.onload(button.click());
+window.addEventListener('load', button.click());
